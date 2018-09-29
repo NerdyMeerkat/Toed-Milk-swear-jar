00183327260d956b31abc156c21cf5f0d4fa5b7c
Simon <simon.bolton86@gmail.com>
2018-09-29 18:03:45 +0100

    Config file created following the template.

---
+++ TwitchBot/Config.py
@@ -0,0 +1,9 @@
+#This is the config file for required twitch data
+
+from Constants import *
+
+HOST = "irc.twitch.tv"
+PORT = 6667
+PASS = "oauth: from https://twitchapps.com/tmi"
+NICK = "Twitch account name of the chat bot"
+CHANNEL = "Channel name for the bot to join"
