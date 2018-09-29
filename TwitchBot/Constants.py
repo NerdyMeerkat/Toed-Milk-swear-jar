98de4e6464aaadcf3690f45d6e46464262960c3e
Simon <simon.bolton86@gmail.com>
2018-09-30 00:29:55 +0100

    Constants data file added.

---
+++ TwitchBot/Constants.py
@@ -0,0 +1,9 @@
+#This file keeps some data required by other parts of the chat bot program
+CARRIAGE_RETURN = "\r\n"
+BUFFER = 1024
+
+#KEYWORDS is the part of the chatbot that will be used to 'recognise' trigger from chat
+KEYWORDS = ["!C-bomb", "!F-bomb"]
+
+#GREETINGS I will implement so people can interact a bit more with the bot
+GREETINGS = []
