6c91dfe67177e10ef10ccbf4003f4a076a516955
Simon <simon.bolton86@gmail.com>
2018-09-30 11:56:37 +0100

    Minor typo fixed.

--- TwitchBot/App/Constants.py
+++ TwitchBot/App/Constants.py
@@ -2,7 +2,7 @@
 CARRIAGE_RETURN = "\r\n"
 BUFFER = 1024
 
-#KEYWORDS is the part of the chatbot that will be used to 'recognise' trigger from chat
+#KEYWORDS is the part of the chatbot that will be used to 'recognise' triggers from chat
 KEYWORDS = ["!C-bomb", "!F-bomb"]
 
 #GREETINGS I will implement so people can interact a bit more with the bot
